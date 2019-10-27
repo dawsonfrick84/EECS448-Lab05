e20f40bb772ffa3ec72a15e467646dc06b383d93
Dawson Frick <d289f187@ku.edu>
2019-10-27 16:03:03 -0500

    Update CreateUser.php

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -1,45 +1,46 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "dawsonfrick84", "oathu4Ai", "dawsonfrick84");
 $user=$_POST["user"];
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s<br>", $mysqli->connect_error);
-    exit();
-}
+if($user!=""){
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s<br>", $mysqli->connect_error);
+      exit();
+  }
 
-$query = "SELECT user_id FROM Users ORDER by user_id";
-$write=true;
+  $query = "SELECT user_id FROM Users ORDER by user_id";
+  $write=true;
 
-if ($result = $mysqli->query($query)) {
-  printf("Connection success<br>Passed in user id: %s<br>", $user);
-    /* fetch associative array */
-    while ($row = $result->fetch_assoc()) {
-        if ($row["user_id"]==$user){
-          $write=false;
+  if ($result = $mysqli->query($query)) {
+    printf("Connection success<br>Passed in user id: %s<br>", $user);
+      while ($row = $result->fetch_assoc()) {
+          if ($row["user_id"]==$user){
+            $write=false;
+          }
+      }
+      if ($write==TRUE){
+        printf("Adding user to database...<br>");
+        $sql = "INSERT INTO Users (user_id) VALUES ('$user')";
+
+        if ($mysqli->query($sql) === TRUE) {
+          printf("User created successfully<br>");
         }
-        // echo "id: " . $row["user_id"]. " <br>"; //show all users
-    }
-    if ($write==TRUE){
-      printf("Adding user to database...<br>");
-      $sql = "INSERT INTO Users (user_id) VALUES ('$user')";
-
-      if ($mysqli->query($sql) === TRUE) {
-        printf("User created successfully<br>");
+        else {
+          printf("Error: " . $sql . "<br>" . $conn->error);
+        }
       }
       else {
-        printf("Error: " . $sql . "<br>" . $conn->error);
+        printf("User already exists!");
       }
-    }
-    else {
-      printf("User already exists!");
-    }
 
 
-    /* free result set */
-    $result->free();
+      $result->free();
+  }
+
+
+
+  $mysqli->close();
+  }
+else{
+  echo "User ID Cannot Be Blank";
 }
-
-
-/* close connection */
-$mysqli->close();
 ?>
