0e6991de680fb281814c87ea18fafb52eabcf35c
Dawson Frick <d289f187@ku.edu>
2019-10-27 16:03:34 -0500

    Update CreatePosts.php

--- Exercise3/CreatePosts.php
+++ Exercise3/CreatePosts.php
@@ -2,46 +2,47 @@
 $mysqli = new mysqli("mysql.eecs.ku.edu", "dawsonfrick84", "oathu4Ai", "dawsonfrick84");
 $user=$_POST["user"];
 $mypost=$_POST["mypost"];
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s<br>", $mysqli->connect_error);
-    exit();
+if($user!=""&&$mypost!=""){
+
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s<br>", $mysqli->connect_error);
+      exit();
+  }
+
+  $query = "SELECT user_id FROM Users ORDER by user_id";
+  $write=FALSE;
+
+  if ($result = $mysqli->query($query)) {
+    printf("Users Connection success<br>Passed in user id: %s<br>", $user);
+
+      while ($row = $result->fetch_assoc()) {
+          if ($row["user_id"]==$user){
+            $write=TRUE;
+          }
+      }
+      if ($write==FALSE){
+        printf("User does not exist<br>");
+      }
+      $result->free();
+  }
+
+  $query = "SELECT post_id, content, author_id FROM Posts ORDER by post_id";
+
+  if ($result = $mysqli->query($query) && $write==TRUE) {
+    printf("Posts Connection success<br>Passed in post: %s<br>", $mypost);
+    $sql = "INSERT INTO Posts (content, author_id) VALUES ('$mypost', '$user')";
+      if ($mysqli->query($sql) === TRUE) {
+        printf("Post created successfully<br>");
+      }
+      else {
+        printf("Error: " . $sql . "<br>" . $conn->error);
+      }
+
+      $result->free();
+  }
+  $mysqli->close();
+  }
+else{
+  echo "User ID/Post content cannot be blank";
 }
-
-$query = "SELECT user_id FROM Users ORDER by user_id";
-$write=FALSE;
-
-if ($result = $mysqli->query($query)) {
-  printf("Users Connection success<br>Passed in user id: %s<br>", $user);
-    /* fetch associative array */
-    while ($row = $result->fetch_assoc()) {
-        if ($row["user_id"]==$user){
-          $write=TRUE;
-        }
-        //echo "id: " . $row["user_id"]. " <br>"; //show all users
-    }
-    if ($write==FALSE){
-      printf("User does not exist<br>");
-    }
-    $result->free();
-}
-
-$query = "SELECT post_id, content, author_id FROM Posts ORDER by post_id";
-
-if ($result = $mysqli->query($query) && $write==TRUE) {
-  printf("Posts Connection success<br>Passed in user id: %s<br>", $user);
-  $sql = "INSERT INTO Posts (content, author_id) VALUES ('$mypost', '$user')";
-    if ($mysqli->query($sql) === TRUE) {
-      printf("Post created successfully<br>");
-    }
-    else {
-      printf("Error: " . $sql . "<br>" . $conn->error);
-    }
-
-    $result->free();
-}
-
-
-/* close connection */
-$mysqli->close();
 ?>
